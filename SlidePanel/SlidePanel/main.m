3f0a32718315f2e4568bc4446eb256a63f2a2c0f
tiger <13900990@qq.com>
2014-02-28 20:25:55 +0800

    project init

---
+++ SlidePanel/SlidePanel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlidePanel
+//
+//  Created by 虫爷 on 14-2-28.
+//  Copyright (c) 2014年 xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SlidePanel/SlidePanelTests/SlidePanelTests.m
@@ -0,0 +1,34 @@
+//
+//  SlidePanelTests.m
+//  SlidePanelTests
+//
+//  Created by 虫爷 on 14-2-28.
+//  Copyright (c) 2014年 xu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SlidePanelTests : XCTestCase
+
+@end
+
+@implementation SlidePanelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
