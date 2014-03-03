d4ebd81a773ad5d732bb95c24df4c84c723808f7
tiger <13900990@qq.com>
2014-03-03 20:39:48 +0800

    dasfasdf

---
+++ SlidePanel/SlidePanel/CommonFunctions.h
@@ -0,0 +1,13 @@
+//
+//  CommonFunctions.h
+//  SlidePanel
+//
+//  Created by 虫爷 on 14-3-3.
+//  Copyright (c) 2014年 xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommonFunctions : NSObject
+
+@end
---
+++ SlidePanel/SlidePanel/CommonFunctions.m
@@ -0,0 +1,13 @@
+//
+//  CommonFunctions.m
+//  SlidePanel
+//
+//  Created by 虫爷 on 14-3-3.
+//  Copyright (c) 2014年 xu. All rights reserved.
+//
+
+#import "CommonFunctions.h"
+
+@implementation CommonFunctions
+
+@end
